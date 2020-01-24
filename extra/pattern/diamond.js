0b781ad121b85896120de4be5324c9283fd0664c
Swapnil Paritosh <swapnil.paritosh@lapr238.successivetech.com>
2020-01-24 18:05:45 +0530

    Refactore

--- extra/pattern/diamond.js
+++ extra/pattern/diamond.js
@@ -1,32 +1,46 @@
-let n =process.argv[2];
-for (let i=1;i<=n;i++)
-{   
-    for(let j=n-1;j>=i;j--)
+let num =process.argv[2];
+function diamond(n)
+{
+    if(n<11 && n>1)
     {
-        process.stdout.write(" ")
+        for (let i=1;i<=n;i++)
+            {   
+                for(let j=n-1;j>=i;j--)
+                {
+                    process.stdout.write(" ")
+
+                }
+                for(let k=1;k<=i;k++)
+                {
+                    process.stdout.write("* ")
+
+                }
+            process.stdout.write("\n")
+            }
+
+
+
+        for (let i=0;i<=n;i++)
+        {   
+            for(let j=1;j<=i;j++)
+            {
+                process.stdout.write(" ")
+
+            }
+            for(let k=n-1;k>=i;k--)
+            {
+                process.stdout.write("* ")
+
+            }
+        process.stdout.write("\n")
+        }
 
     }
-    for(let k=1;k<=i;k++)
-    {
-        process.stdout.write("* ")
 
+    else
+    {
+        process.stdout.write("Enter value between 2 to 10")
     }
-process.stdout.write("\n")
 }
 
-
-
-for (let i=0;i<=n;i++)
-{   
-    for(let j=1;j<=i;j++)
-    {
-        process.stdout.write(" ")
-
-    }
-    for(let k=n-1;k>=i;k--)
-    {
-        process.stdout.write("* ")
-
-    }
-process.stdout.write("\n")
-}
+diamond(num);
--- extra/pattern/equilateral.js
+++ extra/pattern/equilateral.js
@@ -1,16 +1,30 @@
-let n =process.argv[2];
-for (let i=1;i<=n;i++)
-{   
-    for(let j=n-1;j>=i;j--)
+let num =process.argv[2];
+function diamond(n)
+{
+    if(n<11 && n>1)
     {
-        process.stdout.write(" ")
+        for (let i=1;i<=n;i++)
+        {   
+            for(let j=n-1;j>=i;j--)
+            {
+                process.stdout.write(" ")
+
+            }
+            for(let k=1;k<=i;k++)
+            {
+                process.stdout.write("* ")
+
+            }
+        process.stdout.write("\n")
+
+        }
 
     }
-    for(let k=1;k<=i;k++)
+
+    else
     {
-        process.stdout.write("* ")
-
+        process.stdout.write("Enter value between 2 to 10")
     }
-process.stdout.write("\n")
-
 }
+
+diamond(num);
