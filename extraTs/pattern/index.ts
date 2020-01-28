3e71d7685598487c2a573d057dfc2c7fcebcc1a4
Vikas Chauhan <vikas.chauhan@successive.tech>
2020-01-28 20:11:13 +0530

    Revert "feat:Add file"

--- extraTs/pattern/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as diamond } from './diamond'
-export { default as equilateral } from './equilateral'
--- extraTs/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as hasPermission } from './permissions';
-export { default as validateUsers } from './validation';
