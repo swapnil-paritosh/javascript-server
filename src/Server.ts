ba488d963fd7fed5a6e4bdd91e75fb7de718a29e
Swapnil Paritosh <swapnil.paritosh@lapr238.successivetech.com>
2020-01-30 18:33:24 +0530

    feat: add body parser and middleware
    Add body parser and middleware

--- src/Server.ts
+++ src/Server.ts
@@ -1,39 +1,65 @@
-
-import * as express from 'express'
+import * as express from 'express';
+import Iconfig from './config/IConfig';
+import * as bodyParser from 'body-parser';
+import errorHandler from './libs/routes/errorHandler';
+import notFoundRoutes from './libs/routes/notFoundRoute';
+import { Request } from 'express';
 
 class Server {
-    private app: express.Express
+    private app: express.Express;
 
-    constructor(private config) {
+    constructor(private config: Iconfig) {
         this.app = express();
     }
 
     bootstrap = () => {
-        console.log('Inside bootsrap');
+        console.log('Inside Bootstrap');
+        this.initBodyParser();
         this.setupRoutes();
         return this;
     }
+
+    initBodyParser = () => {
+        const { app } = this;
+
+        console.log('Inside init');
+
+        app.use(bodyParser.urlencoded({ extended: false }));
+
+        // parse application/json
+        app.use(bodyParser.json());
+    }
+
     run = () => {
         const { app, config: { port } } = this;
 
-        app.listen(port, (err) => {
+        this.app.listen(this.config.port, (err) => {
             if (err) {
+                console.log('error');
                 throw err;
             }
             console.log('App is running successfully on port ' + port);
-        })
+        });
     }
+
     setupRoutes = () => {
         const { app } = this;
 
-        this.app.get('/health-check', (req, res) => {
+        this.app.get('/health-check', (req: express.Request, res: express.Response) => {
             console.log('Inside health check');
-            res.send('i am ok');
+            res.send('I am OK');
         });
+
+        app.use('/api', (req, res, next) => {
+            console.log('Inside Middleware');
+            console.log(req.body);
+            res.send(req.body);
+        });
+
+        app.use(notFoundRoutes);
+        app.use(errorHandler);
         return this;
-
     }
-
 }
 
 export default Server;
