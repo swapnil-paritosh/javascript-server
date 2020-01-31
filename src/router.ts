ac579db08e16b7bee95a21f599046b00f0b510a3
Swapnil Paritosh <swapnil.paritosh@lapr238.successivetech.com>
2020-01-31 18:01:00 +0530

    feat: Setting of Routes Skeletons #39523

---
+++ src/router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import traineeRouter from './controllers/trainee/routes';
+const mainRouter = Router();
+
+mainRouter.use('/', traineeRouter);
+
+export default mainRouter
